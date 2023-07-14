af8a1ef65fe92865914d3365160d931cac039589
Zeebrow <mzborowski@yahoo.com>
2023-07-14 11:42:29 -0400

    starting point for testing against new pricing page layout

--- tests/test_todo.py
+++ tests/test_todo.py
@@ -62,3 +62,37 @@ def test_do_args_defaults_implement_main_config():
 
 def test_global_scrpr_home():
     assert scrpr.SCRPR_HOME == os.path.join(os.path.expanduser('~'), '.local', 'share', 'scrpr')
+
+
+###########################################################
+# refactored dropdowns + table Selenium operations
+###########################################################
+
+#        # @@@
+#        # TODO put tests where they belong
+#        # test table_functions_map table
+#        # os: Linux
+#        # Region: us-east-1 (Ohio)
+#        assert table_functions_map['get_current_page']() == 1
+#        print(table_functions_map['rows']['get_rows_count_on_page']())
+#        table_functions_map['navto_next_page']()
+#        assert table_functions_map['get_current_page']() == 2
+#        # assert table_functions_map['rows']['get_rows_count_on_page']() == 20
+#        table_functions_map['navto_next_page']()
+#        assert table_functions_map['get_current_page']() == 3
+#        # assert table_functions_map['rows']['get_rows_count_on_page']() == 20
+#        table_functions_map['navto_next_page']()
+#        assert table_functions_map['get_current_page']() == 4
+#        assert table_functions_map['rows']['get_total_row_count']() == 586
+#        assert table_functions_map['get_total_pages']() == 30
+#
+#        for col in ['Instance name', 'On-Demand hourly rate', 'vCPU', 'Memory', 'Storage', 'Network performance']:
+#            assert col in table_functions_map['header']['content']
+#
+#        # [print(r.get_attribute('aria-rowindex')) for r in table_functions_map['rows']['get_rows']()]
+#        # test rows are returned sequentially by using the aria-rowindex attribute (which we can take to be sequential)
+#        for idx, _ in enumerate(c := [ int(row.get_attribute('aria-rowindex')) for row in table_functions_map['rows']['get_rows']() ]):  # noqa: E201,E202
+#            if idx < len(c) - 2:
+#                assert c[idx] == c[idx + 1] - 1
+#
+
